16cf75ddbe49272322cf99c5774dc722848ca683
faekhatami <faekhatami@gmail.com>
2024-07-14 15:20:43 +0330

    Routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Hello from "./Hello";
 import Counter from "./Counter";
 import UserForm from "./UserForm";
@@ -6,13 +7,38 @@ import DataFetcher from "./DataFetcher";
 
 function App() {
   return (
-    <div>
-      <Hello name="React" />
-      <Hello name="World" />
-      <Counter />
-      <UserForm />
-      <DataFetcher />
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <link to="/">Home</link>
+          </li>
+          <li>
+            <link to="/counter">Counter</link>
+          </li>
+          <li>
+            <link to="/form">Form</link>
+          </li>
+          <li>
+            <link to="/fetcher">Fetcher</link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/counter">
+          <Counter />
+        </Route>
+        <Route path="/form">
+          <UserForm />
+        </Route>
+        <Route path="/fetcher">
+          <DataFetcher />
+        </Route>
+        <Route path="/">
+          <Hello name="Router" />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
