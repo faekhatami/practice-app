9c197bb1807fefae825a87df922266369050ffbf
faekhatami <faekhatami@gmail.com>
2024-07-14 12:48:28 +0330

    state practice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Hello from "./Hello";
+import Counter from "./Counter";
 
 function App() {
   return (
     <div>
       <Hello name="React" />
       <Hello name="World" />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Counter />
     </div>
   );
 }
